ba43f308802f255cef6d187d5607aa961705436e
Dolly Birgen <dollybirgen@gmail.com>
2025-09-13 21:58:14 +0300

    Unstaged changes

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+
+# Calculate the sum
+sum_result = number1 + number2
+print(f"Addition of {number1} and {number2} is {sum_result}")
+
+# Calculate the difference
+difference_result = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+
+# Calculate the product
+product_result = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {product_result}")
+
