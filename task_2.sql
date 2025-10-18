d68e02140321fb5f0e2e67ccf4489aa2d2fd727d
Dolly Birgen <dollybirgen@gmail.com>
2025-10-18 16:16:16 +0300

    Adds task_2.sql to create all book store tables

---
+++ task_2.sql
@@ -0,0 +1,44 @@
+-- SWITCH TO THE CORRECT DATABASE
+USE alx_book_store;
+
+-- TABLE 1: AUTHORS
+CREATE TABLE IF NOT EXISTS AUTHORS (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- TABLE 2: BOOKS
+CREATE TABLE IF NOT EXISTS BOOKS (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DECIMAL(10, 2),
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+);
+
+-- TABLE 3: CUSTOMERS
+CREATE TABLE IF NOT EXISTS CUSTOMERS (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+
+-- TABLE 4: ORDERS
+CREATE TABLE IF NOT EXISTS ORDERS (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+);
+
+-- TABLE 5: ORDER DETAILS (The linking table for books and orders)
+CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
+    order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+);
