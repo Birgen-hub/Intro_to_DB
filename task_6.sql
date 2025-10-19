8b964c09c650898e39fd9f3e06782aab537968fb
Dolly Birgen <dollybirgen@gmail.com>
2025-10-19 21:42:27 +0300

    Adds task_6.sql to insert multiple customer rows

---
+++ task_6.sql
@@ -0,0 +1,9 @@
+-- REQUIRED STATEMENT to select the correct database for the checker
+USE alx_book_store;
+
+-- Insert multiple rows into the required 'customer' table (singular, lowercase)
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
